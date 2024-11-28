82b3a8f938a46fd5fa7c6c15c9eca1da9ae626a9
ridera7 <ridera7@gmail.com>
2024-11-28 13:40:43 +0200

    Initial commit

---
+++ src/main/java/com/todolistgpt/TodoListGptApplication.java
@@ -0,0 +1,13 @@
+package com.todolistgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListGptApplication.class, args);
+    }
+
+}
